bc0a1f16f31a8a2d1f1bcc2edfc383dc91b2e0c1
Saurabh Kumar <saurabhkrsingh2001@gmail.com>
2022-03-15 07:40:29 +0000

    added code

--- algoDataBase.py
+++ algoDataBase.py
@@ -1,28 +1,29 @@
 from replit import db
 
 
-def add_algo():
-  db["linear search"] = ["psedo code", "O(n)", "O(1)"]
-  db["binary search"] = ["psedo code",  "O(log n)", "O(1)"]
-  db["jump search"] = ["psedo code", "O(n^-2)", "O(1)"]
-  db["interpolation search"] = ["psedo code", "O(n)", "O(1)"]
-  db["exponential search"] = ["psedo code", "time complexity", "space complexity"]
-  db["ternary search"] = ["psedo code","O(log3 n)", "O(1)"]
+def addAlgo(algo_name):
+  algo_name = algo_name.lower()
+  if algo_name in db.keys():
+    return "Algorithm already in database"
+  else:
+    db[algo_name] = {"time_complexity" : "", "space_complexity" : ""}
+    return "Algorithm added."
 
-  db["selection sort"] = ["psedo code", "O(n^2)", "O(1)"]
-  db["bubble search"] = ["psedo code", "O(n^2)", "O(1)"]
-  db["insertion sort"] = ["psedo code", "O(n^2)", "O(1)"]
-  db["merge sort"] = ["psedo code", "O(nlog n)", "O(1)"]
-  db["heap sort"] = ["psedo code", "time complexity", "space complexity"]
-  db["quick sort"] = ["psedo code", "O(nlog n)", "O(n)"]
-  db["radix sort"] = ["psedo code", "O(nd)", "O(n+2^d)"]
-  db["counting sort"] = ["psedo code", "O(n+k)", "O(n+r)"]
-  db["bucket sort"] = ["psedo code", "O(n^2)", "O(n+k)"]
-  db["shell sort"] = ["psedo code", "O(n^2)", "O(1)"]
-  db["comb sort"] = ["psedo code", "O(n^2)", "O(1)"]
-  db["pigeonhole sort"] = ["psedo code", "O(n+2^k)", "O(2^k)"]
-  db["cycle sort"] = ["psedo code", "O(n^2)", "O(1)"]
+
+def addTimeComplexity(algo_name, time_complexity):
+  db[algo_name]["time_complexity"] = time_complexity
+  return "Time complexity added"
+
+def addSpaceComplexity(algo_name, space_complexity):
+  db[algo_name]["space_complexity"] = space_complexity
+  return "Space complexity added."
 
   
-def clear_db():
-  del db["algos"]
+def delDb():
+  for i in db:
+    del db[i]
+  return "Database deleted."
+
+def delAlgo(algo_name):
+  del db[algo_name]
+  return "Algorithm deleted."
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 from replit import db
 from keep_alive import keep_alive
 from discord.ext import commands,tasks
-from algoDataBase import add_algo,clear_db
+from algoDataBase import *
 
 #bot
 bot = commands.Bot(command_prefix = '?')
@@ -17,23 +17,24 @@ async def hello(ctx):
   await ctx.send("Hello {}".format(ctx.author))
 
   
-#algo list string:
-algo_list = ""
-j = 1
-for i in db.keys():
-  algo_list += str(j) + ". " + i + "\n"
-  j += 1
-
-  
 #algolist command:
-@bot.command(name = "algolist", aliases = ["alglist"], help = "Displays the list of algorithms")
+@bot.command(name = "algolist", help = "Displays the list of algorithms")
 async def algolist(ctx):
-  await ctx.send(">>> " + algo_list)
+  #algo list string:
+  algo_list = ""
+  j = 1
+  for i in db.keys():
+    algo_list += str(j).ljust(2) + '. ' + (i.capitalize()).ljust(25) + db[i]["time_complexity"].ljust(10) + db[i]["space_complexity"].ljust(10) +"\n"
+    j += 1
+    if j > 15:
+      break
+    
+  await ctx.send(algo_list)
 
 
-#pseudo-code command:
-@bot.command(name = "sudocode", aliases = ["sdc"], help = "Displays the pseudo code of algorithms")
-async def sudocode(ctx, *args):
+#display code command:
+@bot.command(name = "code", help = "Displays the code of algorithms")
+async def code(ctx, arg1, *args):
   if len(args) == 0:
     await  ctx.send("**" + "Usage : " +"**" + "`sudocode <algorithm>`" )
     return
@@ -51,9 +52,16 @@ async def sudocode(ctx, *args):
       
     await ctx.send(suggestion)
     return
-    
-  sudo_code = db[algo_name][0]
-  await ctx.send("```" + sudo_code + "```")
+
+  filename = arg1 + ".txt"
+  filepath = 'codes/{}/{}'.format(algo_name, filename)
+  try:
+    f = open(filepath, 'r')
+    code = f.read()
+    await ctx.send("```c++\n" + code + "```")
+  except:
+    await ctx.send("No such code available.")
+
 
 #time complexity command
 @bot.command(name = "timecomplex", aliases = ["tc"], help = "Displays the time complexity")
@@ -86,7 +94,7 @@ async def spacecomplex(ctx, *args):
     return
   algo_name = (' '.join(args)).lower()
 
-  if len(db.prefix(args[0])) == 0:
+  if len(db.prefix(args[0].lower())) == 0:
     await ctx.send(file=discord.File('kyakr.gif'))
     return
 
@@ -94,13 +102,79 @@ async def spacecomplex(ctx, *args):
   if algo_name not in db.keys():
     suggestion = ">>> "+ "_" + "Did you mean: " + "_"
       
-    for i in db.prefix(args[0]):
+    for i in db.prefix(args[0].lower()):
       suggestion += "**" + i + "**" + "\n"
       
     await ctx.send(suggestion)
     return
   space_complexity = db[algo_name][2]
   await ctx.send("```"+"Space Complexity : "+ space_complexity + "```"  )
+
+@bot.command(name = "algo")
+async def algo(ctx, *args):
+  if len(args) == 0:
+    await  ctx.send("**" + "Usage : " +"**" + "`algo <algorithm>`" )
+    return
+  algo_name = (' '.join(args)).lower()
+
+  if len(db.prefix(args[0].lower())) == 0:
+    await ctx.send(file=discord.File('kyakr.gif'))
+    return
+
+  #matching algos
+  if algo_name not in db.keys():
+    suggestion = ">>> "+ "_" + "Did you mean: " + "_"
+      
+    for i in db.prefix(args[0].lower()):
+      suggestion += "**" + i + "**" + "\n"
+      
+    await ctx.send(suggestion)
+    return
+
+  algo_str = ">>> __" + algo_name.capitalize() + "__ \n" + "Time Complexity: " + db[algo_name]["time_complexity"] + "\n" + "Space Complexity: " + db[algo_name]["space_complexity"]
+
+  await ctx.send(algo_str)
+  return
+  
+
+
+#add algorithm
+@bot.command(name = "add_algo", help="add new algorithm")
+async def add_algo(ctx, algo_name):
+  await ctx.send(addAlgo(algo_name))
+
+
+@bot.command(name = "add_time_complexity", aliases = ["atc"], help = "add time complexity of algorithm")
+async def add_time_complexity(ctx, algo_name, time_complexity):
+  await ctx.send(addTimeComplexity(algo_name, time_complexity))
+
+@bot.command(name = "add_space_complexity", aliases = ["asc"], help = "add space complexity")
+async def add_space_complexity(ctx, algo_name, space_complexity):
+  await ctx.send(addSpaceComplexity(algo_name, space_complexity))
+
+
+#algorithm bot logo
+@bot.command(name = "algo_bot_logo")
+async def algo_bot_logo(ctx):
+  await ctx.send(file=discord.File('DigitalFortress.gif'))
+  
+  
+#developers help command
+@bot.command(name = "dev_help")
+async def dev_help(ctx):
+  dev_str = ">>> ___Algorithm Bot___ " + "*Commands:*\n\n" + "Add a new Algorithm : " +'`add_algo "algo_name"`\n' + "Add time complexity : " + '`add_time_complexity "algo name" "time complexity"`\n'+"Add space complexity : " + '`add_space_complexity "algo name" "space complexity"`\n'+"Add space complexity : " + '`add_space_complexity "algo name" "space complexity"`\n'+ "\n*Developed by :* **DigitalFortress**\n\n"
+  await ctx.send(file=discord.File('DigitalFortress.gif'))
+  await ctx.send(dev_str)
+
+
+
+@bot.command(name = "bot_help")
+async def how_to_use_algo_bot(ctx):
+  dev_str = ">>> ___Algorithm Bot___ " + "*Help Commands:*\n"+ "\n>>>___*Search Commands:*___\n" +"Search algorithm list : " + '` algolist`\n'+"Search pseudo code : " + '` sudocode <algo name>`\n'+"Search  time complexity : " + '` timecomplex <algo name>`\n'+"Search  space complexity : " + '` spacecomplex <algo name>`'+ "\n\n>>>___*Devloper Commands:*___\n"+"Add a new Algorithm : " +'`add_algo "algo_name"`\n' + "Add time complexity : " + '`add_time_complexity "algo name" "time complexity"`\n'+ "\n*Developed by :* **DigitalFortress**\n\n"
+  await ctx.send(file=discord.File('DigitalFortress.gif'))
+  await ctx.send(dev_str)
+
+
 
 #flask app
 keep_alive() 
